a1ab8e412b455fc4c2dad54a93e8ff1cf74b1ce6
Chidozie <chidough14@gmail.com>
2023-06-23 11:03:07 +0200

    users bulk delete/change role

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -50,4 +50,53 @@ class AdminController extends Controller
 
         
     }
+
+    public function bulkDeleteUsers (Request $request) {
+
+        if (auth()->user()->role === "admin" || auth()->user()->role === "super admin") {
+            User::whereIn('id', $request->userIds)->delete();
+    
+            return response([
+                'message' => 'Users deleted',
+                'status' => 'success'
+            ], 201);
+         
+        } else {
+            return response([
+                'message' => 'Not Allowed',
+                'status' => 'success'
+            ], 201);
+        }
+    }
+    
+
+    public function bulkUpdateUsers (Request $request) {
+        $arr = [];
+
+        if (auth()->user()->role === "admin" || auth()->user()->role === "super admin") {
+            foreach ($request->userIds as $user) {
+                if ($user['role'] === 'super admin') {
+                  
+                } elseif ($user['role'] === 'admin') {
+                    User::where('id', $user['id'])->update(['role' => 'user']);
+                    $arr[] = User::where('id', $user['id'])->first();
+                } elseif ($user['role'] === 'user') {
+                    User::where('id', $user['id'])->update(['role' => 'admin']);
+                    $arr[] = User::where('id', $user['id'])->first();
+                }
+            }
+    
+            return response([
+                "users"=> $arr,
+                'message' => 'Users roles updated',
+                'status' => 'success'
+            ], 201);
+         
+        } else {
+            return response([
+                'message' => 'Not Allowed',
+                'status' => 'success'
+            ], 201);
+        }
+    }
 }
