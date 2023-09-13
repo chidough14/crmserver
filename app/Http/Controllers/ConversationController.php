1550e07b3489bb8492476379c8cf61aeba328f8d
Chidozie <chidough14@gmail.com>
2023-09-13 11:25:35 +0200

    Admin chats functionality

---
+++ app/Http/Controllers/ConversationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Conversation;
+use Illuminate\Http\Request;
+
+class ConversationController extends Controller
+{
+    public function addConversation (Request $request) {
+        $conversation = Conversation::create([
+            'conversation_string'=> $request->conversation_string,
+            'user_id' => auth()->user()->id
+        ]);
+
+        return response([
+            'conversation'=> $conversation,
+            'message' => 'Conversation created successfully',
+            'status' => 'success'
+        ], 201);
+    }
+
+    public function fetchConversations () {
+        $conversations = Conversation::where("user_id", auth()->user()->id)->get();
+
+        return response([
+            'conversations'=> $conversations,
+            'message' => 'All Conversations',
+            'status' => 'success'
+        ], 201);
+    }
+}
